cd855852d79db15e1ed4e6c9a5c5bc1e2e856544
florvillalba21 <florvllb33@gmail.com>
2024-02-07 10:27:27 -0300

    Se ha quitado verificar si es admin para traer las promociones

--- routes/promotionRoutes.js
+++ routes/promotionRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router()
 
 router.post('/new_promotion', upload.single('promotion-image'), validateNotEmptyFields(['description', 'required_quantity', 'discounted_percentage']), verifyJwt, isAdmin, createPromotion)
 router.put('/update_promotion/:id',  upload.single('promotion-image'), validateNotEmptyFields(['description', 'required_quantity', 'discounted_percentage']), verifyJwt, isAdmin, updatePromotion)
-router.get('/all_promotions', verifyJwt, isAdmin, getPromotions)
+router.get('/all_promotions', verifyJwt, getPromotions)
 router.delete('/delete_promotion/:id', verifyJwt, isAdmin, deletePromotion)
 
 export default router
